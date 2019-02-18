6aca0179e046d30a381669a4d9dec61168ec7d1e
karlbishnu <karlbishnu@gmail.com>
2019-02-18 15:28:47 +0900

    dp 테스트용 소리 파일 추가

--- dp/tests/test_overlay.py
+++ dp/tests/test_overlay.py
@@ -7,8 +7,8 @@ from common.audio.audio_utils import open_audio, graph_spectrogram
 
 class TestOverlay(unittest.TestCase):
     def setUp(self):
-        self.background: AudioSegment = open_audio('test-asset/backgrounds/1.mp3')
-        self.activate: AudioSegment = open_audio('test-asset/activates/3_act2.wav')
+        self.background: AudioSegment = open_audio('test-asset/raw-data/backgrounds/1.mp3')
+        self.activate: AudioSegment = open_audio('test-asset/raw-data/activates/1_2.wav')
 
     def test_overlapped(self):
         self.assertTrue(is_overlapping((2305, 2950), [(824, 1532), (1900, 2305), (3424, 3656)]))
@@ -19,7 +19,7 @@ class TestOverlay(unittest.TestCase):
     @patch('numpy.random.randint', return_value=2254)
     def test_insert_audio_clip(self, rand):
         audio_clip, segment_time = insert_audio_clip(self.background, self.activate, [(3790, 4400)])
-        self.assertEqual((2254, 3169), segment_time)
+        self.assertEqual((2254, 3321), segment_time)
         self.assertEqual(len(self.background), len(audio_clip))
 
     def test_spectrogram(self):
